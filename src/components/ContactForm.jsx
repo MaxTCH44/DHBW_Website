91faeea49e86aba24005f94fae1bc7320b42d1c7
MaxTCH44 <tch.maxime@gmail.com>
2026-05-28 15:12:15 +0200

    :fix: contact page mobile version upgrade

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -136,7 +136,7 @@ export default function ContactForm({ link, label }){
                         minRows={5}
                         required
                         mb="xl"
-                        h="auto"
+                        autosize
                     />
 
                     <Group justify="flex-end">
